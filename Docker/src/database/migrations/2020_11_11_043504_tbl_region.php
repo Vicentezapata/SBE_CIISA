4a867710868124df16d44fd2fcc92a32d49b4343
VicenteZapata <Vicente.zapata@usach.cl>
2020-11-11 03:02:04 -0300

    migraciones

--- Docker/src/database/migrations/2020_11_11_043504_tbl_region.php
+++ Docker/src/database/migrations/2020_11_11_043504_tbl_region.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTypeIngredientsTable extends Migration
+class TblRegion extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,7 @@ class CreateTypeIngredientsTable extends Migration
      */
     public function up()
     {
-        Schema::create('type_ingredients', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -27,6 +23,6 @@ class CreateTypeIngredientsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('type_ingredients');
+        //
     }
 }
--- Docker/src/database/migrations/2020_11_11_043727_tbl_codarea.php
+++ Docker/src/database/migrations/2020_11_11_043727_tbl_codarea.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCocktailsTable extends Migration
+class TblCodarea extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,7 @@ class CreateCocktailsTable extends Migration
      */
     public function up()
     {
-        Schema::create('cocktails', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->integer('degrees');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -28,6 +23,6 @@ class CreateCocktailsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cocktails');
+        //
     }
 }
